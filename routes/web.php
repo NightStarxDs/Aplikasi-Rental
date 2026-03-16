7573e0a0d14c3ac958a8f05d6d49e089bb1fa1a9
NightStarxD <dwiagung0515@gmail.com>
2026-03-16 23:36:29 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,7 @@ use App\Http\Controllers\HomeController;
 // });
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/contact', [HomeController::class, 'contact'])->name('contact');
+
+Route::get('/welcome', function () {
+    return view('welcome');
+});
