0cc7a9c2e220702bc092fa2a88bcc134b929d6e0
fauzan najib_ali <fauzan.btm06@gmail.com>
2026-04-14 21:22:53 +0700

    Setup: Routes for every view

---
+++ resources/views/DataBarang.blade.php
@@ -0,0 +1,30 @@
+<div>
+    <html>
+<head>
+    <title>Data Table</title>
+</head>
+<body>
+
+<table border="1">
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Nama</th>
+            <th>Harga</th>
+        </tr>
+    </thead>
+
+    <tbody>
+        @foreach($data as $dataku)
+        <tr>
+            <td>{{ $dataku['id'] }}</td>
+            <td>{{ $dataku['nama'] }}</td>
+            <td>{{ $dataku['harga'] }}</td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
+
+</body>
+</html>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 require __DIR__ . '/auth.php';
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DataBarang;
 
 Route::get('/', function () {
     return view('welcome');
@@ -20,4 +21,60 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
+Route::get('/DataBarang', [DataBarang::class, 'tampilkan']);
+
+Route::get('/Admin_Edit_Barang', function () {
+    return view('Admin.Admin_Edit_Barang');
+});
+
+Route::get('/Admin_Edit_User', function () {
+    return view('Admin.Admin_Edit_User');
+});
+
+Route::get('/Admin_Inventaris_Barang', function () {
+    return view('Admin.Admin_Inventaris_Barang');
+});
+
+Route::get('/Admin_kelola_Penyewaan', function () {
+    return view('Admin.Admin_kelola_Penyewaan');
+});
+
+Route::get('/Admin_Edit_User', function () {
+    return view('Admin.Admin_Edit_User');
+});
+
+Route::get('/Admin_Riwayat_Pelanggan', function () {
+    return view('Admin.Admin_Riwayat_Pelanggan');
+});
+
+Route::get('/Admin_Tambah_User', function () {
+    return view('Admin.Admin_Tambah_User');
+});
+
+Route::get('/Admin_Transaksi_Penyewaan', function () {
+    return view('Admin.Admin_Transaksi_Penyewaan');
+});
+
+Route::get('/Admin_Edit_Barang', function () {
+    return view('Admin.Admin_Edit_Barang');
+});
+
+// Pelanggan
+Route::get('/Halaman_Checkout', function () {
+    return view('User.Halaman_Checkout');
+});
+
+Route::get('/Halaman_Keranjang', function () {
+    return view('User.Halaman_Keranjang');
+});
+
+Route::get('/Halaman_Penjualan', function () {
+    return view('User.Halaman_Penjualan');
+});
+
+Route::get('/Profil_Pelanggan', function () {
+    return view('User.Profil_Pelanggan');
 });
