5f765d73c360b026ab81007dc93f10c6a61343ce
Dwi Agung Willy Anto <63480839+NightStarxDs@users.noreply.github.com>
2026-05-06 16:47:20 +0700

    Update secondary-button.blade.php

--- resources/views/components/secondary-button.blade.php
+++ resources/views/components/secondary-button.blade.php
@@ -1,3 +1,3 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'border border-emerald-400 font-medium text-white bg-transparent transition-all hover:bg-emerald/10 hover:border-emerald-200 focus:outline-none focus:ring-2 focus:ring-emerald-400 active:scale-95']) }}>
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'border border-emerald-400 font-medium text-green bg-transparent transition-all hover:bg-emerald/10 hover:border-emerald-200 focus:outline-none focus:ring-2 focus:ring-emerald-400 active:scale-95']) }}>
     {{ $slot }}
 </button>
